87e41ed409641175405a2f10cf9858841326a90a
Id Avila <i.avilacamargo@ugto.mx>
2021-03-04 02:12:51 -0600

    new view

---
+++ view/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-app>
+    <Header/>
+  </v-app>
+</template>
+
+<script>
+import Header from '@/components/Header';
+
+export default {
+  name: 'App',
+
+  components: {
+    //HelloWorld,
+    Home
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ view/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-card class="overflow-hidden">
+    <v-app-bar
+      absolute
+      color="#00BFA6"
+      elevate-on-scroll
+      scroll-target="#scrolling-techniques-7"
+    >
+      <v-img
+        src="../assets/heart.png"
+        contain="true"
+        height="50"
+        position="left"
+        ></v-img>
+      <v-toolbar-title>Medical Planner</v-toolbar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    </v-app-bar>
+    <v-sheet
+      id="scrolling-techniques-7"
+      class="overflow-y-auto"
+      max-height="600"
+    >
+      <v-container style="height: 1500px;"> </v-container>
+    </v-sheet>
+  </v-card>
+</template>
